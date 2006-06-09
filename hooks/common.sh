dd424464f5b71405f3226c2cb863d0a2bc445a4c
steve <none@none>
2006-06-09 10:16:12 +0000

    2006-06-09 10:16:12 by steve   Added.

---
+++ hooks/common.sh
@@ -0,0 +1,76 @@
+#
+#  Common functions which may be used by any hook script
+#
+#
+
+
+#
+#  If we're running verbosely show a message, otherwise swallow it.
+#
+function logMessage
+{
+    message="$*"
+
+    if [ ! -z "${verbose}" ]; then
+	echo $message
+    fi
+}
+
+
+
+#
+#  Install a Debian package via apt-get.
+#
+function installDebianPackage
+{
+    prefix=$1
+    package=$2
+
+    #
+    # Log our options
+    #
+    logMessage "Installing Debian package ${package} to prefix ${prefix}"
+
+    #
+    # Make sure the prefix exists.
+    #
+    if [ -z "${prefix}" ]; then
+	logMessage "Prefix '${prefix}' does not exist.  Abortin"
+	return
+    fi
+
+    #
+    # Install the package
+    #
+    DEBIAN_FRONTEND=noninteractive chroot ${prefix} /usr/bin/apt-get --yes --force-yes install ${package}
+
+}
+
+
+
+#
+#  Install a CentOS4 package via yum
+#
+function installCentOS4Package
+{
+    prefix=$1
+    package=$2
+
+    #
+    # Log our options
+    #
+    logMessage "Installing CentOS4 ${package} to prefix ${prefix}"
+
+    #
+    # Make sure the prefix exists.
+    #
+    if [ -z "${prefix}" ]; then
+	logMessage "Prefix '${prefix}' does not exist.  Abortin"
+	return
+    fi
+
+    #
+    # Install the package
+    #
+    chroot ${prefix} /usr/bin/yum -y install ${package}
+}
