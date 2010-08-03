73528d94e70d8b6cf04491a095e63c015d0b1413
Axel Beckert <abe@deuxchevaux.org>
2010-08-03 15:51:33 +0200

    Mounts also /dev/pts automatically before running any hooks or rules
    Closes Debian bug #588783.

--- hooks/common.sh
+++ hooks/common.sh
@@ -98,9 +98,7 @@ installDebianPackage ()
     #
     # Install the packages
     #
-    mount -t devpts devpts ${prefix}/dev/pts
     DEBIAN_FRONTEND=noninteractive chroot ${prefix} /usr/bin/apt-get --yes --force-yes install "$@"
-    umount ${prefix}/dev/pts
 
     #
     #  Remove the policy-rc.d script.
